d29b66d8a2ab70799c805460ae2c69033b29db96
Süleyman SEZER <me.iamcodder@gmail.com>
2020-10-12 22:37:55 +0300

    Theme code edited

--- PreferenceExample/app/src/main/java/com/patronusstudio/preferenceexample/SettingsFragment.kt
+++ PreferenceExample/app/src/main/java/com/patronusstudio/preferenceexample/SettingsFragment.kt
@@ -10,42 +10,59 @@ import androidx.preference.SwitchPreferenceCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    private val LIGHT_THEME = "Light Theme"
+    private val DARK_THEME = "Dark Theme"
+    private val PATRONUS_WEB = "http://www.patronusstudio.com/"
+
+    private var isLightMode = true
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        val lightTheme = findPreference<SwitchPreferenceCompat>("appThemeLight")
-        val darkTheme = findPreference<SwitchPreferenceCompat>("appThemeDark")
+        val lightTheme = findPreference<SwitchPreferenceCompat>("appThemeLight")!!
+        val darkTheme = findPreference<SwitchPreferenceCompat>("appThemeDark")!!
 
-        lightTheme?.setOnPreferenceChangeListener { _, newValue ->
-            if (newValue == true) {
-                darkTheme?.isChecked = false
-                setTheme(false)
-            } else {
-                darkTheme?.isChecked = true
-                setTheme(true)
-            }
+
+        lightTheme.setOnPreferenceChangeListener { _, newValue ->
+            val currentSwitch = newValue == true
+            setSwitchButton(currentSwitch, lightTheme, darkTheme)
+            setTheme()
             true
         }
 
-
-        darkTheme?.setOnPreferenceChangeListener { _, newValue ->
-
-            if (newValue == true) {
-                lightTheme?.isChecked = false
-                setTheme(true)
-            } else {
-                lightTheme?.isChecked = true
-                setTheme(false)
-            }
-
+        darkTheme.setOnPreferenceChangeListener { _, newValue ->
+            val currentSwitch = newValue == true
+            setSwitchButton(currentSwitch, darkTheme, lightTheme)
+            setTheme()
             true
         }
 
+        val webPreference = findPreference<Preference>("webPage")
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(PATRONUS_WEB)
+        webPreference!!.intent = intent
 
     }
 
-    private fun setTheme(isDarkActive: Boolean) {
-        if (isDarkActive) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    private fun setSwitchButton(
+        currentSwitch: Boolean,
+        currentTheme: SwitchPreferenceCompat,
+        otherTheme: SwitchPreferenceCompat
+    ) {
+        if (currentSwitch) {
+            currentTheme.isChecked = true
+            otherTheme.isChecked = false
+            isLightMode = currentTheme.toString() == DARK_THEME
+        } else {
+            currentTheme.isChecked = false
+            otherTheme.isChecked = true
+            isLightMode = currentTheme.toString() == LIGHT_THEME
+        }
+
+    }
+
+    private fun setTheme() {
+        if (isLightMode) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
     }
